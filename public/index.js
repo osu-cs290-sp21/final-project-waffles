9d18558a139e0dfb4c7d17aad5c771fea874f741
ghimh <ghimh@oregonstate.edu>
2021-06-06 18:01:02 -0700

    i think its the variables

--- public/index.js
+++ public/index.js
@@ -1,34 +1,30 @@
-var allRecipes = []; // all recipes
+var allRecipeElems = []; // all recipes
 
-function showCreateRecipeModal() {
+function showCreateRecipe() {
+	var modalBackdrop = document.getElementById("modal-backdrop");  
 	var createRecipeModal = document.getElementById("create-recipe-modal");
-	var modalBackdrop = document.getElementById("modal-backdrop");
-	
 	createRecipeModal.classList.remove('hidden');
 	modalBackdrop.classList.remove('hidden');
 }
 
-function closeCreateRecipeModal() {
+function closeCreateRecipe() {
+	var modalBackdrop = document.getElementById("modal-backdrop");  
 	var createRecipeModal = document.getElementById("create-recipe-modal");
-	var modalBackdrop = document.getElementById("modal-backdrop");
-	
 	createrecipeModal.classList.add("hidden");
 	modalBackdrop.classList.add("hidden");
-	
+
 	clearRecipeInputValues();
 }
 
 function clearRecipeInputValues() {
-	var recipeInputs = document.getElementsByClassName("recipe-input-element");
-	var recipeNameInput = document.getElementById("recipe-name-input");
-	var recipeAuthorInput = document.getElementById("recipe-author-element");
-  var recipeTextInput = document.getElementById("recipe-text-input");
-	recipeNameInput.value = "";
-  recipeAuthorInput = "";
-	recipeTextInput = "";
+	var recipeInputElems = document.getElementsByClassName("recipe-input-element");
+  for (var i = 0; i < recipeInputElems.length; i++) {
+    var input = recipeInputElems[i].querySelector('input, textarea');
+    input.value = '';
+  }
 }
 
-function creatNewRecipe(name, author, servings, bakeTime, description, ingredients, instructions, notes) {
+function NewRecipeElems(name, author, servings, bakeTime, description, ingredients, instructions, notes) {
 	var recipeData = {
     name: recipeName,
     author: recipeAuthor,
@@ -41,84 +37,87 @@ function creatNewRecipe(name, author, servings, bakeTime, description, ingredien
 	};
 	return recipeData;
 }
-
-function insertNewRecipe() {
-	var recipeName = document.getElementById("recipe-name-input").value;
-	var recipeAuthor = document.getElementById("recipe-authors-input").value;
-  var recipeServings = document.getElementById("recipe-serving-input").value;
-  var recipeBakeTime = document.getElementById("recipe-baketime-input").value;
-  var recipeDescription = document.getElementById("recipe-desciption-input").value;
-  var recipeIngredients = document.getElementById("recipe-ingredients-input").value;
-  var recipeInstructions = document.getElementById("recipe-instructions-input").value;
-  var recipeNotes = document.getElementById("recipe-notes-input").value;
-  var addNewRecipe = createNewRecipe(recipeName. recipeAuthor, recipeServings, recipeBakeTime, recipeDescription, recipeIngredients, recipeInstructions, recipeNotes)
-
-	storeRecipe(newRecipe, function (err) {
-		if(err) {
-			alert("[ERROR]: Unable to upload recipe to database. Received:\n\n" + err);
-		}
-		else {
-			var templateArgs = {
-				name: recipeName,
-				author: recipeAuthor,
-        description: recipeDescription,
-			};				
-		}
-	});
-	closeCreateRecipeModal(); // bye bye
-}
-	
-	
-/* [add function to make sure all recipe boxes are filled] */
-
-
-function storeRecipe(recipeData, callback) {
-	var postURL = "/addRecipe";
-	var postRequest = new XMLHttpRequest();
-	postRequest.open('POST',postURL);
-	postRequest.setRequestHeader('Content-Type','RecipeData/json'); // right .json file?
-	
-	postRequest.addEventListener('load',function(event) {
-		var error;
-		if(event.target.status != 200) {
-			error = event.target.response;
-		}
-		callback(error);
-	});
-	
-	postRequest.send(JSON.stringify(recipeData));
+function getrecipeId() { //idk about this one
+  var pathComponents = window.location.pathname.split('/');
+  if (pathComponents[0] !== '' && pathComponents[1] !== 'recipes') {
+    return null;
+  }
+return pathComponents[2];
 }
 
+
+	/* [add function to make sure all recipe boxes are filled] */
+
+
+  function insertNewRecipe(recipeName, recipeAuthor) {
+    var recipeName = document.getElementById('recipe-name-input').value || ''; //name
+    var recipeAuthor = document.getElementById('recipe-author-input').value || ''; //author
+  
+    if (recipeName && recipeAuthor) {
+      var recipeID= getrecipeid();
+      if (recipeID) {
+        console.log("== recipe ID:", recipeID);
+        storeRecipe(recipeID, recipeName, recipeAuthor, function (err) {
+          if (err) {
+            alert("Unable to save recipe. Got this error:\n\n" + err);
+          }
+          else {
+            var recipeTemplate = Handlebars.templates.recipe;
+            var templateArgs = {
+              name: recipeName,
+              author: recipeAuthor
+          };
+          var recipeHTML = recipeTemplate(templateArgs);
+          var recipeContainer = document.querySelector('.recipe-container');
+          recipeContainer.insertAdjacentHTML('beforeend', recipeHTML);
+          }
+        });
+      }
+      closeCreateRecipeModal();
+    }
+    else {
+      alert('Please enter both "name" and "author"!');
+    }
+  var postBody = {
+    name: recipeName,
+    author: recipeAuthor
+    };
+  postRequest.send(JSON.stringify(postBody));
+  }
 
 
 function recipeSearch() {
 	var searchQuery = document.getElementById("navbar-search-input").value;
 	searchQuery = searcyQuery ? searchQuery.trim().toLowerCase : '';
-	
 	var recipeContainer = document.querySelector(".recipe-container");
 	while (recipeContainer.lastChild) {
 		recipeContainer.removeChild(recipeContainer.lastChild);
 	}
 	
-	allRecipes.forEach(function (recipeName) {
-		if (!searchQuery || recipeName.textContent.toLowerCase().indexOf(searchQuery) !== -1) {
-			recipeContainer.appendChild(recipeName);
+	allRecipesElems.forEach(function (recipeElem) {
+		if (!searchQuery || recipeElem.textContent.toLowerCase().indexOf(searchQuery) !== -1) {
+			recipeContainer.appendChild(recipeElem);
 		}
 	});
 }
 
 window.addEventListener("DOMContentLoaded", function () {	
+  var recipeElemsCollection = document.getElementsByClassName('recipe-container');
+  for (var i = 0; i < recipeElemsCollection.length; i++) {
+    allRecipeElems.push(recipeElemsCollection[i]);
+  }
+
 	var createRecipeButton = document.getElementById('create-recipe-button');
-	createRecipeButton.addEventListener('click', showCreateRecipeModal);
+	createRecipeButton.addEventListener('click', showCreateRecipe);
 	
 	var modalCloseButton = document.getElementsByClassName("modal-close-button");
-	modalCloseButton[0].addEventListener("click", closeCreateRecipeModal);
+	modalCloseButton[0].addEventListener("click", closeCreateRecipe);
 	
 	var modalCancelButton = document.getElementsByClassName("modal-cancel-button");
-	modalCancelButton[0].addEventListener("click", closeCreateRecipeModal);
+	modalCancelButton[0].addEventListener("click", closeCreateRecipe);
 	
 	var modalAcceptButton = document.getElementsByClassName("modal-accept-button");
-	modalAcceptButton[0].addEventListener("click", insertNewRecipePost);
+	modalAcceptButton[0].addEventListener("click", insertNewRecip);
 	
 	var searchButton = document.getElementById("navbar-search-button");
 	searchButton.addEventListener("click", recipeSearch);
@@ -126,3 +125,4 @@ window.addEventListener("DOMContentLoaded", function () {
 	var searchInput = document.getElementById("navbar-search-input");
 	searchInput.addEventListener("input", recipeSearch);
 });
+
